b99f10c4a0fe7ce592eb02fe36b7d347e91810cc
phoferek-del <phoferek@gmail.com>
2025-11-11 14:33:00 +0100

    Create __manifest__.py
    Add manifest for custom_inventory

---
+++ custom_inventory/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Inventory',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Prázdny modul pre Skysize nasadenie',
+    'author': 'Peter Hoferek',
+    'depends': ['stock'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
